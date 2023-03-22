2362fd1cd9704d8c8d7e47281c8d59b17971bb20
Sarbjot Singh <sarbjots341@gmail.com>
2023-03-22 10:15:02 -0600

    card and image added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+
+
 function App() {
   return (
-    <Cards style={{ width: '19rem'}}>
-      <Cards.Img variant= "top" src="assets/2.jpg=Imagecap"/>
-    </Cards>
+    
+    <Card style={{ width: '20rem' }}>
+      <img src="./assets/2.jpg" alt="" />
+      <Card.Body>
+        <Card.Title>Tandoori Chicken</Card.Title>
+        <Card.Text>
+                 Some quick example guyide for the tanbdoori chicken recipe.
+        </Card.Text>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>Ingriendients</ListGroup.Item>
+        <ListGroup.Item>Water</ListGroup.Item>
+        <ListGroup.Item>Cooking time</ListGroup.Item>
+      </ListGroup>
+      <Card.Body>
+    
+        <Card.Link href="https">Recipe Link</Card.Link>
+      </Card.Body>
+    </Card>
   );
 }
 
